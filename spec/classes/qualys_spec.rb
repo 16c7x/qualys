976ea8f847d20eb389b4550d9b866f0f9ac7e906
Andrew Jones <pic16c7x@gmail.com>
2021-02-01 17:12:53 +0000

    Update qualys_spec.rb

--- spec/classes/qualys_spec.rb
+++ spec/classes/qualys_spec.rb
@@ -6,7 +6,12 @@ describe 'qualys' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
-
+      let(:params) do 
+        { 
+          activationid: '12345',
+          customerid: '6789',
+        }
+      end
       it { is_expected.to contain_class('qualys') }
     end
   end
