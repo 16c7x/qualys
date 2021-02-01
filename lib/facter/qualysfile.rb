e7d53a1db0472e8fc3597766cd202b8a44be96fd
Andrew Jones <pic16c7x@gmail.com>
2021-02-01 16:25:30 +0000

    Update qualysfile.rb

--- lib/facter/qualysfile.rb
+++ lib/facter/qualysfile.rb
@@ -1,18 +1,18 @@
-# Creates the fact qualysfile which is a hash of the key=value pairs in the 
+# Creates the fact qualysfile which is a hash of the key=value pairs in the
 # /etc/qualys/cloud-agent/qualys-cloud-agent.conf file
 # Reads the file line by line, ignoring comment lines and blank lines.
 # Splits the line at the = and adds both sides to the hash.
-# Removes commas and line endings. 
+# Removes commas and line endings.
 Facter.add('qualysfile') do
-  confine :kernel => 'Linux'
+  confine kernel: 'Linux'
   setcode do
     qualysfile = {}
     filecontents = File.open('/etc/qualys/cloud-agent/qualys-cloud-agent.conf').read
     filecontents.each_line do |line|
-      if line.include? "#"
+      if line.include? '#'
       elsif line.to_s.strip.empty?
       else
-        a = line.split("=")
+        a = line.split('=')
         qualysfile[a[0]] = a[1].chomp
       end
     end
