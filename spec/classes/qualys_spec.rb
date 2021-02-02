d2311e707e9bfd93a347eb4a13d9aba2a0e3e937
Andrew Jones <pic16c7x@gmail.com>
2021-02-02 11:24:40 +0000

    Update qualys_spec.rb

--- spec/classes/qualys_spec.rb
+++ spec/classes/qualys_spec.rb
@@ -5,28 +5,7 @@ require 'spec_helper'
 describe 'qualys' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
-      let(:facts) do # { os_facts }
-        {
-          os_facts
-          qualysfile: {
-            UseSudo: '0',
-            sudouser: 'root',
-            sudocommand: 'sudo',
-            log_level:  '3',
-            logfiledir: '/var/log/qualys',
-            cmdmaxtimeout: '1800',
-            processpriority: '0',
-            requesttimeout: '600',
-            useauditdispatcher: '1',
-            cmdstdoutsize: '1024',
-            luascripttimeout: '3600',
-            activationid: '12345',
-            customerid: '6789',
-            usergroup: 'root',
-          }
-        }
-      end
-
+      let(:facts) do { os_facts }
       let(:params) do
         {
           activationid: '12345',
